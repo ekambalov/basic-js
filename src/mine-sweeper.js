a90d1459440c238dc2b2835e74af617bc911338e
ekambalov <emakcomp@gmail.com>
2022-05-02 15:13:38 +0200

    feat: mine-sweeper

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -23,9 +23,41 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ function minesweeper(matrix) {
+
+  let newMatrix = [];
+  for( let i = 0; i<matrix.length+2; i++){
+    newMatrix[i]=[];
+    for(let j = 0; j<matrix[0].length+2; j++){
+      if(i==0||i==matrix.length+1||j==0||j==matrix[0].length+1) {
+        newMatrix[i][j]=false;
+      } else{
+        newMatrix[i][j]=matrix[i-1][j-1];
+      }
+    }
+  }
+  let field = [];
+  for (let i = 0; i < matrix.length; i++) {
+    field[i]=[];    
+  }
+
+  for (let i = 1; i < newMatrix.length-1; i++) {
+    for (let j = 1; j < newMatrix[i].length-1; j++) {
+      let bNum=0;
+      
+         if(newMatrix[i-1][j-1]) bNum+=1;
+         if(newMatrix[i-1][j] )bNum+=1;
+         if(newMatrix[i-1][j+1])bNum+=1;
+         if(newMatrix[i][j-1] )bNum+=1;
+         if(newMatrix[i][j+1])bNum+=1;
+         if(newMatrix[i+1][j-1]) bNum+=1;
+         if(newMatrix[i+1][j])bNum+=1;
+         if(newMatrix[i+1][j+1]) bNum+=1;
+
+        field[i-1][j-1]=bNum;
+    }
+  }
+return field
 }
 
 module.exports = {
